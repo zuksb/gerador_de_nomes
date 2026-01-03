09625532c09897f659e0e057b362d55a79340293
zuksb <davihenriquegamesoliveira.com@gmail.com>
2026-01-03 14:55:15 -0300

    adição da 1.1, adicionei opçao de lugares e nomes alternativos

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import random
 
-gn = ["Miyu", "Sari", "Yuna", "Hana", "Kira"]
-bn = ["Toren", "Kato", "Guran", "Ryu", "Hanzo"]
+girl_name = ["Miyu", "Sari", "Yuna", "Hana", "Kira"]
 
-uc = str(input("selecione o genero do seu nome, feminino ou masculino ")).lower()
+alt_names1 = ["ka","mi","ra","shi","yu"]
+alt_names2 = ["ran","yara","sharan","koyara","wu","Ryuri"]
 
-if uc == "feminino":
-    guc = random.choice(gn)
-    print(guc)
+boys_name = ["Toren", "Kato", "Guran", "Ryu", "Hanzo"]
 
-elif uc =="masculino":
-    buc = random.choice(bn)
-    print(buc)
+name_place1 = ["Kan", "Sun", "Sen", "Par", "tarke", "koitsu", "Chin","saze"]
+name_place2 = ["ka","rin","mi","ra","shi","yu"]
+
+user_choice = str(input("Selecione o tipo de nome que voce quer(masculino, feminino, lugar ou alternativo ")).lower()
+
+if user_choice == "feminino":
+    name_chose_girl = random.choice(girl_name)
+    print(name_chose_girl)
+
+elif user_choice =="masculino":
+    name_chose_boys = random.choice(boys_name)
+    print(name_chose_boys)
+
+elif user_choice == "lugar":
+    place_name_chose = random.choice(name_place1) + random.choice(name_place2)
+    print(place_name_chose)
+
+elif user_choice == "alternativo":
+    name_chose_alt = random.choice(alt_names1) + random.choice(alt_names2)
+    print(name_chose_alt)
 
 else:
-    print("Digite feminino ou masculino!")
+    print("A opção não esta disponivel!")
 
