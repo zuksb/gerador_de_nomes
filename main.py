4aef4744eb429eecafa1764d8e6aa6b40c5b67f0
zuksb <davihenriquegamesoliveira.com@gmail.com>
2026-01-09 12:12:13 -0300

    adicionei ele em loop

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 import random
 
-girl_name = ["Miyu", "Sari", "Yuna", "Hana", "Kira"]
+while True:
 
-alt_names1 = ["ka","mi","ra","shi","yu"]
-alt_names2 = ["ran","yara","sharan","koyara","wu","Ryuri"]
+    girl_name = ["Miyu", "Sari", "Yuna", "Hana", "Kira"]
 
-boys_name = ["Toren", "Kato", "Guran", "Ryu", "Hanzo"]
+    alt_names1 = ["ka","mi","ra","shi","yu"]
+    alt_names2 = ["ran","yara","sharan","koyara","wu","Ryuri"]
 
-name_place1 = ["Kan", "Sun", "Sen", "Par", "tarke", "koitsu", "Chin","saze"]
-name_place2 = ["ka","rin","mi","ra","shi","yu"]
+    boys_name = ["Toren", "Kato", "Guran", "Ryu", "Hanzo"]
 
-user_choice = str(input("Selecione o tipo de nome que voce quer(masculino, feminino, lugar ou alternativo ")).lower()
+    name_place1 = ["Kan", "Sun", "Sen", "Par", "tarke", "koitsu", "Chin","saze"]
+    name_place2 = ["ka","rin","mi","ra","shi","yu"]
 
-if user_choice == "feminino":
-    name_chose_girl = random.choice(girl_name)
-    print(name_chose_girl)
+    user_choice = str(input("Selecione o tipo de nome que voce quer(masculino, feminino, lugar ou alternativo ")).lower()
 
-elif user_choice =="masculino":
-    name_chose_boys = random.choice(boys_name)
-    print(name_chose_boys)
+    if user_choice == "feminino":
+        name_chose_girl = random.choice(girl_name)
+        print(name_chose_girl)
 
-elif user_choice == "lugar":
-    place_name_chose = random.choice(name_place1) + random.choice(name_place2)
-    print(place_name_chose)
+    elif user_choice =="masculino":
+        name_chose_boys = random.choice(boys_name)
+        print(name_chose_boys)
 
-elif user_choice == "alternativo":
-    name_chose_alt = random.choice(alt_names1) + random.choice(alt_names2)
-    print(name_chose_alt)
+    elif user_choice == "lugar":
+        place_name_chose = random.choice(name_place1) + random.choice(name_place2)
+        print(place_name_chose)
 
-else:
-    print("A opção não esta disponivel!")
+    elif user_choice == "alternativo":
+        name_chose_alt = random.choice(alt_names1) + random.choice(alt_names2)
+        print(name_chose_alt)
 
+    else:
+        print("A opção não esta disponivel!")
+
+
+    if user_choice == "sair":
+        print('ate logo, tenha um otimo dia')
+        break
+
+    print()
